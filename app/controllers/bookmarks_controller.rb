c127e2a629bee7bbe202f477918b1bcd227c5620
Felipe Lisperguer <felipe.lisperguer.h@gmail.com>
2022-05-05 18:35:22 -0400

    Ejercicio casi listo falta create

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,25 +1,32 @@
 class BookmarksController < ApplicationController
 
   def new
-    @bookmark = Bookmark.new(params[:id])
+    # @list = List.find(params[list_id])
+    #Revisar bien el porqué de lo anterior
+    @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to bookmark_path(@bookmark)
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
   
-  def bookmark_params
-    params.require(:bookmark).permit(:comment)
-  end
+
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.delete
     redirect_to bookmark_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
   end
 end
